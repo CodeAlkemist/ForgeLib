cad764004b88563ccedcf5a9c53dff081011ccca
Matheus Silva <matheus@codealkemy.co>
2017-07-02 16:41:03 -0300

    ForgeLib build 1.0.0a-0x001
    Signed-off-by: Matheus Silva <matheus@codealkemy.co>

--- src/Stack.c
+++ src/Stack.c
@@ -4,8 +4,7 @@
  * @file Stack.h
  */
 
-#include <stdlib.h>
-#include "Config.h"
+#include "Stack.h"
 
 /**
  * @brief Standard FILO stack with a traverse function to get any specific index.
@@ -13,62 +12,46 @@
  */
 
 /**
- *	@struct stack
- */
-struct stack
-{
-	/**
-	 * To determine if it will be the first pop on the stack, only mess with if you know what you are doing
-	 * @memberof stack
-	 */
-	bool popc;
-	/**
-	 * Top of the stack index use with @code stack.data[stack.top] @endcode
-	 * to get what's at the top of the stack
-	 * @memberof stack
-	 */
-	int top;
-	/**
-	 * The size of the stack
-	 * @memberof stack
-	 */
-	int size;
-	/**
-	 * The capacity of the stack
-	 * @memberof stack
-	 */
-	int capacity;
-	/**
-	 *The actual data held in the stack
-	 * @memberof stack
-	 */
-	int *data;
-};
-/**
- * @memberof stack
- */
-typedef struct stack Stack;
-
-/**
  * @brief Initialises the stack with a given size
  * @param[in] stack the stack to initialise
  * @param[in] size size to initialise the stack with
  * @memberof stack
  */
-void stack_init(Stack *stack, int size);
+void stack_init(Stack *stack, int size)
+{
+	/* Initialise size and capacity */
+	stack->size = 1;
+	stack->capacity = size;
+	stack->top = 0;
+	stack->popc = true;
+	/* Allocate memory */
+	stack->data = malloc(SIZE_OF_INT * stack->capacity);
+}
 
 /**
  * @brief Frees the memory allocated to the given stack
  * @param[in] stack
  * @memberof stack
  */
-void stack_free(Stack *stack);
+void stack_free(Stack *stack)
+{
+	free(stack);
+}
 
 /**
  * @brief Sanitises the the memory previously occupied by the stack
  * @param stack to sanitise
  */
-void sanitise_free(Stack *stack);
+void stack_sanitised_free(Stack *stack)
+{
+	while(stack->top >= 0)
+	{
+		stack->data[stack->top] = 0xFFFFFF;
+		stack->top--;
+	}
+	free(stack);
+}
+
 
 /**
  * @brief Resizes the stack
@@ -77,7 +60,12 @@ void sanitise_free(Stack *stack);
  * @param[in] size the size to go with the stack
  * @memberof stack
  */
-void stack_resize(Stack *stack, int size);
+void stack_resize(Stack *stack, int size)
+{
+	if(size < 1) return;
+	stack->capacity += size;
+	stack->data = realloc(stack->data, SIZE_OF_INT * stack->capacity);
+}
 
 /**
  * @brief Pushes a value into the stack
@@ -85,7 +73,15 @@ void stack_resize(Stack *stack, int size);
  * @param[in] data	any value that can be casted into an integer
  * @memberof stack
  */
-void stack_push(Stack *stack, int data);
+void stack_push(Stack *stack, int data)
+{
+	if((stack->size + 1) >= stack->capacity){
+		stack_resize(stack, 1);
+	}
+	stack->size++;
+	/* Add data to the top of the stack */
+	stack->data[stack->top++] = data;
+}
 
 /**
  * @brief Pops a value from the stack
@@ -93,7 +89,19 @@ void stack_push(Stack *stack, int data);
  * @param[in] stack the stack to pop a value from
  * @return the topmost data on the stack
  */
-int stack_pop(Stack *stack);
+int stack_pop(Stack *stack)
+{
+	if(stack->popc)
+	{
+		stack->top--;
+		stack->size--;
+		stack->popc = false;
+	}
+	int tmp = stack->data[stack->top];
+	stack->top--;
+	stack->size--;
+	return tmp;
+}
 
 /**
  * @deprecated
@@ -101,7 +109,10 @@ int stack_pop(Stack *stack);
  * @param[in] stack
  * @return
  */
-int stack_check(Stack *stack);
+int stack_check(Stack *stack)
+{
+	return stack->data[stack->top];
+}
 
 /**
  * @deprecated Will be removed on next major, use:
@@ -113,7 +124,10 @@ int stack_check(Stack *stack);
  * @return stack size
  * @memberof stack
  */
-int getsizeof_stack(Stack *stack);
+int getsizeof_stack(Stack *stack)
+{
+	return stack->size;
+}
 
 /**
  * @brief Merges two stacks together
@@ -122,7 +136,17 @@ int getsizeof_stack(Stack *stack);
  * @param[in] stack1 Stack to be merged onto
  * @param[out] stack2 Stack to be merged into
  */
-void stack_merge(Stack *stack1, Stack *stack2);
+void stack_merge(Stack *stack1, Stack *stack2)
+{
+	if(stack1->size > 0)
+	{
+		while(stack1->size < 0)
+		{
+			stack_push(stack2, stack_pop(stack1));
+		}
+		stack_free(stack1);
+	}
+}
 
 /**
  * @brief Traverse the stack to the desired index
@@ -130,4 +154,11 @@ void stack_merge(Stack *stack1, Stack *stack2);
  * @param[in] index index to traverse to
  * @return traversed index contents in case of error returns 0xBADBEEF
  */
-int stack_traverse(Stack *stack, int index);
+int stack_traverse(Stack *stack, int index)
+{
+	if(index > stack->size || index < 0)
+	{
+		return 0xBADBEEF;
+	}
+	return stack->data[index];
+}
---
+++ src/to_rpn.c
@@ -0,0 +1,90 @@
+/**
+ * Shunting yard algorithm Copyright (c) 2017 Matheus Silva
+ * Algorithm to convert from infix notation to RPN (Postfix)
+ * Takes 2 times the size of the input in ram to work
+ */
+
+#include "libforge.h"
+/**
+ * Return the operator precedence
+ */
+int op_precedence(char op)
+{
+	if(op == 0x5E)
+	{
+		return 4;
+	}
+	else if(op == 0x2A || op == 0x2F)
+	{
+		return 3;
+	}
+	else if(op == 0x2B || op == 0x2D)
+	{
+		return 2;
+	}
+	return 0;
+}
+
+Stack to_rpn(char **raw)
+{
+	Stack out;
+	stack_init(&out, (int)sizeof(raw));
+	Stack ops;
+	stack_init(&ops, 10);
+	/* Create error stack just in case but left it uninitialised */
+	Stack err;
+	int pc = 0x0000;
+	while (pc <= (int)sizeof(raw) - 1)
+	{
+		/* Get next char from the input */
+		char cc = (int)raw[pc];
+		/* Check for null */
+		if (cc == 0x00) {
+
+		}
+		/* If it is a number push it to the output stack */
+		if (cc >= 0x30 && cc <= 0x39)
+		{
+			stack_push(&out, cc);
+		}
+		/* If not check if it is an operator*/
+		else if (cc >= 0x2A && cc <=0x2F)
+		{
+			if(ops.size > 0)
+			{
+				char tsop = stack_check(&ops);
+				op_precedence(cc) > op_precedence(tsop) ? stack_push(&ops, cc) : stack_push(&out,stack_pop(&ops));
+			}
+			else
+			{
+				stack_push(&ops, cc);
+			}
+		}
+		/* Check if it is left parenthesis*/
+		else if(cc == 0x28)
+		{
+			/* While there are operators and they are not a right parenthesis keep popping operators */
+			while(ops.size >= 0 && stack_check(&ops) != 0x29)
+			{
+				stack_push(&out, stack_pop(&ops));
+			}
+			if(ops.size == 0)
+			{
+				stack_init(&err, 10);
+				stack_push(&err, 0xBAD1);
+				return err;
+			}
+		}
+		/* If it is the last character check if there are still any operators left in the stack and pop all of them */
+		else if(pc == (int)sizeof(raw) - 1)
+		{
+			while(ops.size > 0)
+			{
+				stack_push(&out, stack_pop(&ops));
+			}
+		}
+		/* Finally increment the counter */
+		pc++;
+	}
+	return out;
+}
